96c8808858bada5c2296ca505727cb5d6a815c54
Nils Gerke <social.nge@mailbox.org>
2020-04-17 21:26:24 +0200

    inlcude only needed feeds

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 sudo chown $(id -u):$(id -g) .
 git clone https://git.openwrt.org/openwrt/openwrt.git openwrt
 cd openwrt
-./scripts/feeds update -a
+./scripts/feeds update
 ./scripts/feeds install -a -p luci
 ./scripts/feeds install -a -p packages
-./scripts/feeds install -a -p routing
-./scripts/feeds install -a -p telephony
 cp ../openwrt-apu/.config-apu2-docker .config
 make defconfig
 make -j`nproc`
