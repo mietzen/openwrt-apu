a142544fca7175c2bff322a77228a7f2fd2dc9f6
Nils Gerke <social.nge@mailbox.org>
2020-04-19 17:50:42 +0200

    try building all

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 sudo chown -R $(id -u):$(id -g) .
 cd openwrt
-./scripts/feeds update
+./scripts/feeds update -a
 ./scripts/feeds install -a -p luci
 ./scripts/feeds install -a -p packages
+./scripts/feeds install -a -p routing
+./scripts/feeds install -a -p telephony
 cp ../openwrt-apu/.config-apu2-docker .config
 make defconfig
-make -j`nproc`
+IGNORE_ERRORS=1
+make -j`nproc` download check world
 exit 0
