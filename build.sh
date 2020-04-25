456d0e97df719c6c6e4a4224d873d43deb53a882
Nils Gerke <social.nge@mailbox.org>
2020-04-25 15:55:43 +0200

    build last snapshot

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 sudo chown -R $(id -u):$(id -g) .
 cd openwrt
-git checkout $(curl -s https://downloads.openwrt.org/snapshots/targets/x86/64/version.buildinfo | cut -d'-' -f2)
-rm -f feeds.conf.default
-curl -s -o feeds.conf.default https://downloads.openwrt.org/snapshots/targets/x86/64/feeds.buildinfo
-rm -f .config
 ./scripts/feeds update
 ./scripts/feeds install -a
 git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
