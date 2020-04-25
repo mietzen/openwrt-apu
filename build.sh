9bd5e03d9488fab91dd00900540a68c0311ddb78
Nils Gerke <social.nge@mailbox.org>
2020-04-25 18:25:12 +0200

    build argon

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ sudo chown -R $(id -u):$(id -g) .
 cd openwrt
 ./scripts/feeds update
 ./scripts/feeds install -a
-git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 cp ../openwrt-apu/.config-apu2-image .config
 make defconfig
 make -j`nproc` download world 2>&1 | tee ../build.log
