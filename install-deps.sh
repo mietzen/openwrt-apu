1cd0fc591447606c06a28bc0f53b08fe38046b33
Nils Gerke <social.nge@mailbox.org>
2020-10-24 22:26:31 +0200

    use bintray

--- install-deps.sh
+++ install-deps.sh
@@ -2,6 +2,6 @@
 sudo apt update -q
 sudo apt-get install subversion build-essential libncurses5-dev \
     zlib1g-dev gawk git ccache gettext libssl-dev xsltproc zip \
-    libncursesw5-dev python unzip -q -y
+    libncursesw5-dev python unzip jq tree -q -y
 
 exit 0
